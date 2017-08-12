0fd865e8e5d90e04f990dfb78545bdf95d639ef6
ebrahim <ebrahim.poursadeqi@gmail.com>
2017-08-12 16:11:57 +0430

    first commi for codecept

---
+++ tests/unit/HelloTest.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class HelloTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+        codecept_debug('Hello, Im from debug mode');
+        $this->assertTrue(true);
+    }
+}
